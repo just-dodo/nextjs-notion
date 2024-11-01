ec51ccf78284dbd311335e73baa67b21ebec86ff
Travis Fischer <fisch0920@gmail.com>
2024-10-31 20:49:33 -0500

    fix: locally linked

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,5 @@
-import * as React from 'react'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-
 import { IconContext } from '@react-icons/all-files'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 import { manifest } from '@/lib/config'
 
